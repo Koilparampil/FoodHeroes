024b57780e4cc917a303b1bd3c2efa39ef36dfae
Soheab Khan <skhangns@gmail.com>
2022-03-03 14:06:19 -0500

    set up database

--- config/database.js
+++ config/database.js
@@ -1,11 +1,12 @@
 // config/database.js
-require('dotenv').config();
+require("dotenv").config();
 module.exports = {
-    'connection': {
-        'host': 'localhost',
-        'user': process.env.DB_USER,
-        'password': process.env.DB_PASSWORD
-    },
-	'database': process.env.DB_NAME,
-    'users_table': 'users'
+  connection: {
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: 3306,
+  },
+  database: process.env.DB_NAME,
+  users_table: "users",
 };
