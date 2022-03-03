ec12941502c6517285934df8d24c3ac02fec0a6b
KreshsDesktop <ckresh99@gmail.com>
2022-03-02 21:58:30 -0500

    initial commit

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'Plokmn0978=-'
+    },
+	'database': 'my_schema',
+    'users_table': 'users'
+};
