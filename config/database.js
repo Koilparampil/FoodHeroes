c650808849228a39e93dfb5e45474211664e829d
llting592 <llting592@gmail.com>
2022-03-04 12:00:50 -0500

    welcome page changes

--- config/database.js
+++ config/database.js
@@ -2,9 +2,10 @@
 require('dotenv').config();
 module.exports = {
     'connection': {
-        'host': 'localhost',
-        'user': process.env.DB_USER,
-        'password': process.env.DB_PASSWORD
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PW,
+        port: 3306
     },
 	'database': process.env.DB_NAME,
     'users_table': 'users'
