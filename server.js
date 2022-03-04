2296160bec4d75ae84df31466a516204343dac1c
Soheab Khan <skhangns@gmail.com>
2022-03-04 11:54:01 -0500

    fixing merge issues

--- server.js
+++ server.js
@@ -2,48 +2,49 @@
 
 // set up ======================================================================
 // get all the tools we need
-var express  = require('express');
-var session  = require('express-session');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
-var app      = express();
-var port     = process.env.PORT || 8080;
-require('./auth');
-var passport = require('passport');
-var flash    = require('connect-flash');
+var express = require("express");
+var session = require("express-session");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var morgan = require("morgan");
+var app = express();
+var port = process.env.PORT || 8080;
+require("./auth");
+var passport = require("passport");
+var flash = require("connect-flash");
 
 // configuration ===============================================================
 // connect to our database
 
-require('./config/passport')(passport); // pass passport for configuration
-
-
+require("./config/passport")(passport); // pass passport for configuration
 
 // set up our express application
-app.use(morgan('dev')); // log every request to the console
+app.use(morgan("dev")); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
-app.set('view engine', 'ejs'); // set up ejs for templating
+app.set("view engine", "ejs"); // set up ejs for templating
 
 // required for passport
-app.use(session({
-	secret: process.env.SESSION_SECRET,
-	resave: true,
-	saveUninitialized: true
- } )); // session secret
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 // routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require("./app/routes.js")(app, passport); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
-app.listen(port, ()=> console.log(`listening on port ${port}`));
-
+app.listen(port, () => console.log(`listening on port ${port}`));
