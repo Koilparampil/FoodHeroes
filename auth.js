b604bff9a55fd169aa9e85d3f68e1e643a266101
KreshsDesktop <ckresh99@gmail.com>
2022-03-07 14:40:18 -0500

    changed the callback URL to an ENV variable

--- auth.js
+++ auth.js
@@ -11,7 +11,7 @@ connection.query('USE ' + dbconfig.database);
 passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/google/callback",
+    callbackURL: process.env.DOMAIN+"/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
